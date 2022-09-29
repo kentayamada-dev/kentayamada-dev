831da8c690a447245aa65b8e39feb00e250b13de
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2022-09-29 04:15:50 +0000

    update

--- main.py
+++ main.py
@@ -9,10 +9,96 @@ from my_logger import MyLogger
 
 CURRENT_DATETIME = environ["CURRENT_DATETIME"]
 
+my_logger = MyLogger()
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(60))
-def get_yt_id(get_yt_id_logger):
-    logger = get_yt_id_logger.get_logger()
+
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(1))
+def get_url(path, text, key):
+    logger = my_logger.get_logger()
+    log = f"Key  : {key}" f"\nText : {text}" f"\nPath : {path}"
+
+    try:
+        response = HTMLSession().get(
+            f"https://www.youtube.com/{path}/videos?view=2&sort=dd&live_view=501&shelf_id=0"
+        )
+        response.html.render(sleep=1)
+        url = next(
+            iter(response.html.find("a#video-title", containing=text, first=True).links)
+        )
+
+        logger.error(log)
+    except Exception as exc:
+        logger.critical(log)
+        raise exc
+
+    return url
+
+
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(1))
+def get_weather_data(key, loc):
+    logger = my_logger.get_logger()
+    log = f"Key : {key}" f"\nLoc : {loc}"
+
+    try:
+        response = HTMLSession().get(f"https://www.google.com/search?q={loc}+天気")
+        response.html.render(sleep=1)
+        find = response.html.find
+        temperature = round((int(find("span#wob_tm", first=True).text) - 32) / 1.8)
+        icon_url = f'https://{find("img#wob_tci", first=True).attrs["src"]}'
+        humidity = find("span#wob_hm", first=True).text
+        ms_wind = round(
+            int(find("span#wob_ws", first=True).text.split(" ")[0]) * 0.44704
+        )
+        wind = f"{ms_wind}m/s"
+
+        logger.error(log)
+    except Exception as exc:
+        logger.critical(log)
+        raise exc
+
+    return temperature, icon_url, humidity, wind
+
+
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(3))
+def save_image(yt_id, quality, key):
+    logger = my_logger.get_logger()
+    driver, user_agent = get_chrome_driver()
+    log = f"UA    : {user_agent}" f"\nKey   : {key}" f"\nYT ID : {yt_id}"
+
+    try:
+        driver.get(f"https://www.youtube.com/embed/{yt_id}?rel=0&html5=1&autoplay=1")
+        driver.set_window_size(960, 540)
+        width = driver.execute_script("return document.body.scrollWidth")
+        height = driver.execute_script("return document.body.scrollHeight")
+        driver.set_window_size(width, height)
+
+        sleep(3)
+        driver.find_element(By.XPATH, '//button[@aria-label="Play"]').click()
+
+        sleep(3)
+        driver.find_element(By.CLASS_NAME, "ytp-settings-button").click()
+
+        sleep(3)
+        driver.find_element(
+            By.XPATH, '//div[@class="ytp-menuitem"]/div[text()="Quality"]'
+        ).click()
+
+        sleep(3)
+        driver.find_element(By.XPATH, f'//span[contains(text(),"{quality}")]').click()
+
+        sleep(3)
+        image = f"{key}_{CURRENT_DATETIME}.png"
+        driver.save_screenshot(f"assets_temp/{image}")
+
+        logger.error(log)
+    except Exception as exc:
+        logger.critical(log)
+        raise exc
+
+    return image
+
+
+def get_yt_id():
     dynamic_live_cam_list = [
         {"path": "c/HAKODATELIVECAMERA", "key": "hakodate", "text": "函館駅前ライブカメラ②"},
         {
@@ -84,127 +170,41 @@ def get_yt_id(get_yt_id_logger):
     }
 
     for data in dynamic_live_cam_list:
-        log = (
-            f"Key  : {data['key']}"
-            f"\nText : {data['text']}"
-            f"\nPath : {data['path']}"
-        )
-
-        try:
-            response = HTMLSession().get(
-                f"https://www.youtube.com/{data['path']}/videos?view=2&sort=dd&live_view=501&shelf_id=0"
-            )
-            response.html.render()
-            found_url = next(
-                iter(
-                    response.html.find(
-                        "a#video-title", containing=data["text"], first=True
-                    ).links
-                )
-            )
-            target_char = "="
-            idx = found_url.find(target_char)
-            yt_id = found_url[idx + len(target_char) :]
-            initial_list[data["key"]]["id"] = yt_id
-
-            logger.error(log)
-        except Exception as exc:
-            logger.critical(log)
-            raise exc
+        found_url = get_url(data["path"], data["text"], data["key"])
+        target_char = "="
+        idx = found_url.find(target_char)
+        yt_id = found_url[idx + len(target_char) :]
+        initial_list[data["key"]]["id"] = yt_id
 
     return initial_list
 
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(60))
-def get_live_cam_list(live_cam_logger, initial_live_cam_list):
-    logger = live_cam_logger.get_logger()
-    chrome_driver, user_agent = get_chrome_driver()
-
+def save_live_cam_list(initial_live_cam_list):
     for key, value in initial_live_cam_list.items():
-        log = f"UA    : {user_agent}" f"\nKey   : {key}" f"\nYT ID : {value['id']}"
-
-        try:
-            chrome_driver.get(
-                f"https://www.youtube.com/embed/{value['id']}?rel=0&html5=1&autoplay=1"
-            )
-            chrome_driver.set_window_size(960, 540)
-            width = chrome_driver.execute_script("return document.body.scrollWidth")
-            height = chrome_driver.execute_script("return document.body.scrollHeight")
-            chrome_driver.set_window_size(width, height)
-
-            sleep(3)
-            chrome_driver.find_element(By.XPATH, '//button[@aria-label="Play"]').click()
-
-            sleep(3)
-            chrome_driver.find_element(By.CLASS_NAME, "ytp-settings-button").click()
-
-            sleep(3)
-            chrome_driver.find_element(
-                By.XPATH, '//div[@class="ytp-menuitem"]/div[text()="Quality"]'
-            ).click()
-
-            sleep(3)
-            chrome_driver.find_element(
-                By.XPATH, f'//span[contains(text(),"{value["quality"]}")]'
-            ).click()
-
-            sleep(3)
-            image = f"{key}_{CURRENT_DATETIME}.png"
-            chrome_driver.save_screenshot(f"assets_temp/{image}")
-            value["img"] = image
-
-            logger.error(log)
-        except Exception as exc:
-            logger.critical(log)
-            raise exc
+        value["img"] = save_image(value["id"], value["quality"], key)
 
     return initial_live_cam_list
 
 
-@retry(stop=stop_after_attempt(5), wait=wait_fixed(60))
-def get_weather_data(weather_logger, updated_cam_list):
-    logger = weather_logger.get_logger()
-
+def save_weather_data(updated_cam_list):
     for key, value in updated_cam_list.items():
-        log = f"Key : {key}" f"\nLoc : {value['weather']['loc']}"
-
-        try:
-            response = HTMLSession().get(
-                f"https://www.google.com/search?q={value['weather']['loc']}+天気"
-            )
-            response.html.render()
-            find = response.html.find
-            value["temperature"] = round(
-                (int(find("span#wob_tm", first=True).text) - 32) / 1.8
-            )
-            value[
-                "icon_url"
-            ] = f'https://{find("img#wob_tci", first=True).attrs["src"]}'
-            value["humidity"] = find("span#wob_hm", first=True).text
-            ms_wind = round(
-                int(find("span#wob_ws", first=True).text.split(" ")[0]) * 0.44704
-            )
-            value["wind"] = f"{ms_wind}m/s"
-
-            logger.error(log)
-        except Exception as exc:
-            logger.critical(log)
-            raise exc
+        (
+            value["temperature"],
+            value["icon_url"],
+            value["humidity"],
+            value["wind"],
+        ) = get_weather_data(key, value["weather"]["loc"])
 
     return updated_cam_list
 
 
 if __name__ == "__main__":
-    my_logger = MyLogger()
     mkdir("assets_temp")
     env = Environment(loader=FileSystemLoader("."))
     template = env.get_template("README.tpl")
     current_datetime = CURRENT_DATETIME.split("_")
     updated_date = f"{current_datetime[0].replace('-', '/')} {current_datetime[1].replace('-', ':')}"
-    live_cam_list = get_weather_data(
-        my_logger,
-        get_live_cam_list(my_logger, get_yt_id(my_logger)),
-    )
+    live_cam_list = save_weather_data(save_live_cam_list(get_yt_id()))
 
     with open("README.md", "w", encoding="utf-8") as file:
         file.write(
