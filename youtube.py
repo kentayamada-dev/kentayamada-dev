417d29fd020b0094889f59b62d44103c38fa0050
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2022-10-05 14:58:14 +0000

    test

--- youtube.py
+++ youtube.py
@@ -6,6 +6,18 @@ from playwright.sync_api import sync_playwright
 class YouTube:
     YOUTUBE_URL = "https://www.youtube.com"
 
+    @staticmethod
+    def __crop_center(pil_img: str, crop_width: int, crop_height: int):
+        img_width, img_height = pil_img.size
+        return pil_img.crop(
+            (
+                (img_width - crop_width) // 2,
+                (img_height - crop_height) // 2,
+                (img_width + crop_width) // 2,
+                (img_height + crop_height) // 2,
+            )
+        )
+
     @classmethod
     def get_video_id(cls, channel_path: str, video_title: str):
         response = HTMLSession().get(
@@ -42,10 +54,8 @@ class YouTube:
                 executable_path="/usr/bin/google-chrome-stable"
             )
             page = browser.new_page()
-            page.set_viewport_size({"width": 960, "height": 540})
-            page.goto(
-                f"{cls.YOUTUBE_URL}/embed/{video_id}?rel=0&html5=1&autoplay=1"
-            )
+            page.set_viewport_size({"width": 1920, "height": 1080})
+            page.goto(f"{cls.YOUTUBE_URL}/embed/{video_id}?rel=0&html5=1&autoplay=1")
             page.wait_for_timeout(3000)
             page.locator("button.ytp-play-button").click()
             page.wait_for_timeout(3000)
@@ -58,6 +68,8 @@ class YouTube:
             page.screenshot(path=temp_image_path)
             browser.close()
 
-        Image.open(temp_image_path).save(video_capture_path, quality=100, method=6)
+        initial_image = Image.open(temp_image_path)
+        cropped_image = cls.__crop_center(initial_image, 480, 270)
+        cropped_image.save(video_capture_path, quality=100, method=6)
 
         return video_capture_path
