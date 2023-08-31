e551648beee5283f5667ca456fd6ae9e270e99fd
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2023-08-31 04:43:22 +0000

    test3

--- .github/workflows/actions/delete-old-workflow.js
+++ .github/workflows/actions/delete-old-workflow.js
@@ -1,25 +1,29 @@
 import { Octokit } from "@octokit/core";
+import fetch from "node-fetch";
 
 export const deleteOldWorkflow = async (authKey) => {
-    const octokit = new Octokit({
-        auth: authKey
-      });
-      
-      const asd = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
-        owner: "kentayamada-dev",
-        repo: "kentayamada-dev",
-        per_page: 2,
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      });
-      
-      await octokit.request("DELETE /repos/{owner}/{repo}/actions/runs/{run_id}", {
-        owner: "kentayamada-dev",
-        repo: "kentayamada-dev",
-        run_id: asd["data"]["workflow_runs"][1]["id"],
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      });
-  };
+  const octokit = new Octokit({
+    request: {
+      fetch: fetch,
+    },
+    auth: authKey,
+  });
+
+  const asd = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
+    owner: "kentayamada-dev",
+    repo: "kentayamada-dev",
+    per_page: 2,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+
+  await octokit.request("DELETE /repos/{owner}/{repo}/actions/runs/{run_id}", {
+    owner: "kentayamada-dev",
+    repo: "kentayamada-dev",
+    run_id: asd["data"]["workflow_runs"][1]["id"],
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+};
