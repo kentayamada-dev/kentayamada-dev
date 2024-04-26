463609b9e99f4084ded6e9a6034d99ac3a353ecc
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2024-04-26 18:05:29 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -35,25 +35,26 @@ class Weather:
             case _:
                 return "not-available.svg"
 
+    @staticmethod
+    def __extract_value(data_list, suffix):
+        return next((item.replace(suffix, "") for item in data_list if item.endswith(suffix)), None)
+
     @classmethod
     def get_weather_data(cls, query: str):
         my_logger = MyLogger().get_logger()
         url = f"https://weathernews.jp/onebox/{query}"
         response = HTMLSession().get(url)
         find = response.html.find  # type: ignore
-        data = find("ul.weather-now__ul>li")
-        raw_temperature = data[1].text
-        raw_weather = data[0].text
-        raw_humidity = data[2].text
-        raw_wind = data[4].text
-        temperature = raw_temperature.replace("気温", "").replace("℃", "")
+        data = find('div.nowWeather', first=True).text.split()
+        temperature = cls.__extract_value(data,'℃'))
+        weather = data[0]
+        humidity = cls.__extract_value(data,'%'))
+        wind = cls.__extract_value(data,'m/s'))
+        wind_direction = data[13]
         is_night = environ["CURRENT_DATETIME"].split("_")[1].split("-")[0] >= "18"
-        icon = cls.__get_weather_icon(raw_weather.replace("天気", ""), is_night)
-        humidity = raw_humidity.replace("湿度", "").replace("%", "")
-        wind_direction, wind_ms = raw_wind.replace("風　", "").split()
-        wind = wind_ms.replace("m/s", "")
+        icon = cls.__get_weather_icon(weather, is_night)
 
-        log = f"{raw_temperature}\n{raw_weather}\n{raw_humidity}\n{raw_wind}"
+        log = f"{temperature}\n{weather}\n{humidity}\n{wind}\n{wind_direction}"
         my_logger.error(log)
 
         return (temperature, icon, humidity, wind_direction, wind, url)
