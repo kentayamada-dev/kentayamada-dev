52a3f0edf1df1df9550a81972d97d9b16a47d057
Kenta Yamada <kentayamada058@gmail.com>
2024-07-13 22:56:18 -0400

    update

--- portfolio/app/constants/fonts/index.ts
+++ portfolio/app/constants/fonts/index.ts
@@ -3,14 +3,18 @@ import { Noto_Sans_JP, Roboto_Mono } from 'next/font/google';
 
 const notoSansJP = Noto_Sans_JP({
   display: 'swap',
+  style: ['normal'],
   subsets: ['latin'],
-  variable: '--font-noto-sans-jp'
+  variable: '--font-noto-sans-jp',
+  weight: ['400']
 });
 
 const robotoMono = Roboto_Mono({
   display: 'swap',
+  style: ['normal'],
   subsets: ['latin'],
-  variable: '--font-roboto-mono'
+  variable: '--font-roboto-mono',
+  weight: ['400']
 });
 
 const fonts = `${notoSansJP.variable} ${robotoMono.variable}`;
