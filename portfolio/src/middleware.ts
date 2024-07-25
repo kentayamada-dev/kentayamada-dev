1c780d90a1dbe44b983273245b693c3c2c42b541
Kenta Yamada <kentayamada058@gmail.com>
2024-07-25 15:19:10 -0400

    update

--- portfolio/src/middleware.ts
+++ portfolio/src/middleware.ts
@@ -5,7 +5,15 @@ import type { MiddlewareConfig, NextMiddleware, NextRequest } from 'next/server'
 
 // eslint-disable-next-line custom/as-const-satisfies
 export const config: MiddlewareConfig = {
-  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)']
+  // eslint-disable-next-line @typescript-eslint/lines-around-comment
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
 
 // eslint-disable-next-line @typescript-eslint/padding-line-between-statements
