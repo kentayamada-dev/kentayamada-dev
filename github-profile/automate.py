2536c84d22ee3176d3799dc84f38653ed182b6af
Kenta Yamada <kentayamada058@gmail.com>
2024-08-05 23:50:46 -0400

    update

--- github-profile/automate.py
+++ github-profile/automate.py
@@ -95,8 +95,8 @@ class Automate:
         async with async_playwright() as playwright:
             browser = await playwright.chromium.launch(executable_path=self.EXECUTABLE_PATH)
             context = await browser.new_context(java_script_enabled=False)
+            page = await context.new_page()
             try:
-                page = await context.new_page()
                 url = f"https://weathernews.jp/onebox/{weather_init['query']}"
                 await page.goto(url=url, timeout=0)
                 data = str(await page.locator("div.nowWeather").text_content()).split()
@@ -117,8 +117,7 @@ class Automate:
             except Exception as e:  # noqa: BLE001
                 self.logger.error(f"Error: {e}")  # noqa: G004, TRY400
             finally:
-                await browser.close()
-                await context.close()
+                await page.close()
 
         self.logger.debug(weather_info)
         weather_init.update(weather_info)
@@ -154,8 +153,8 @@ class Automate:
             context = await browser.new_context(
                 viewport={"width": view_width, "height": view_height},
             )
+            page = await context.new_page()
             try:
-                page = await context.new_page()
                 await page.goto(url=f'{youtube_url}/{youtube["path"]}/streams', timeout=0)
                 video_id = str(await page.get_by_title(f'{youtube["title"]}').nth(0).get_attribute("href")).split("=")[
                     -1
@@ -181,8 +180,7 @@ class Automate:
                 self.logger.error(f"Error: {e}")  # noqa: G004, TRY400
                 youtube["img_path"] = "./github-profile/static/404.gif"
             finally:
-                await browser.close()
-                await context.close()
+                await page.close()
 
     @classmethod
     def __get_clip(cls, view_width: int, view_height: int) -> FloatRect:
