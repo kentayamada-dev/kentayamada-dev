6b0017fb45907cef4a689cb660fa3754a8273fac
Kenta Yamada <kentayamada058@gmail.com>
2025-02-19 01:05:39 -0500

    update

--- portfolio/src/app/components/elements/notFoundAnimation/index.tsx
+++ portfolio/src/app/components/elements/notFoundAnimation/index.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import Lottie from 'lottie-react';
+import dynamic from 'next/dynamic';
 import notFoundAnimation from './not-found.json';
 import type { NotFoundAnimationType } from './types';
+
+const Lottie = dynamic(async () => {
+  // eslint-disable-next-line import/dynamic-import-chunkname
+  return import('lottie-react');
+});
 
 const NotFoundAnimation: NotFoundAnimationType = () => {
   return <Lottie animationData={notFoundAnimation} className='h-[inherit]' loop />;
