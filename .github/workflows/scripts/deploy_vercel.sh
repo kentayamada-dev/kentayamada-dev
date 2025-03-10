ceba4c2271db85431f8a206f93341e786b9e2e29
Kenta Yamada <kentayamada058@gmail.com>
2025-03-10 15:36:54 -0400

    test 2

--- .github/workflows/scripts/deploy_vercel.sh
+++ .github/workflows/scripts/deploy_vercel.sh
@@ -5,5 +5,18 @@ curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=$2 SHELL=/bin/bash 
 pnpm env use --global $1
 cd ./app/portfolio
 vercel pull --yes --environment=production --token=$3
-vercel build --prod --token=$3
+
+while true; do
+  echo "Attempting vercel build..."
+  vercel build --prod --token="$3"
+
+  if [ $? -eq 0 ]; then
+    echo "Vercel build succeeded!"
+    break
+  else
+    echo "Vercel build failed. Retrying in 10 seconds..."
+    sleep 10
+  fi
+done
+
 vercel deploy --prebuilt --prod --token=$3
