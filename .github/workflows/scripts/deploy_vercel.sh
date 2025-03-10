b2d3f34b2d47f9ed6882a6cafb65f51c14bf621e
Kenta Yamada <kentayamada058@gmail.com>
2025-03-10 15:49:07 -0400

    update

--- .github/workflows/scripts/deploy_vercel.sh
+++ .github/workflows/scripts/deploy_vercel.sh
@@ -5,19 +5,5 @@ curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=$2 SHELL=/bin/bash 
 pnpm env use --global $1
 cd ./app/portfolio
 vercel pull --yes --environment=production --token=$3
-
-while true; do
-  echo "Attempting vercel build..."
-  vercel build --prod --token="$3"
-  exit 1
-
-  if [ $? -eq 0 ]; then
-    echo "Vercel build succeeded!"
-    break
-  else
-    echo "Vercel build failed. Retrying in 10 seconds..."
-    sleep 10
-  fi
-done
-
+vercel build --prod --token=$3
 vercel deploy --prebuilt --prod --token=$3
