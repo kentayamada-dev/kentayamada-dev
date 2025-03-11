ad750e40fe0e5767ee906137599877d2274f33ed
Kenta Yamada <kentayamada058@gmail.com>
2025-03-11 03:06:46 -0400

    update

--- portfolio/husky-pre-commit.sh
+++ portfolio/husky-pre-commit.sh
@@ -1,9 +1,16 @@
-DIR="./portfolio"
+PORTFOLIO_DIR="./portfolio"
+PORTFOLIO_SRC_DIR="./portfolio/src"
 
-if git diff --cached --quiet -- $DIR; then
-    echo "No changes in $DIR, skipping pre-commit."
-    exit 0
+if git diff --cached --quiet -- "$PORTFOLIO_DIR"; then
+  echo "No changes in $PORTFOLIO_DIR, skipping pre-commit."
 else
-    echo "Changes detected in $DIR, running pre-commit script."
-    cd portfolio && pnpm test && pnpm knip && pnpm exec lint-staged
+  echo "Changes detected in $PORTFOLIO_DIR, running pre-commit script."
+  cd "$PORTFOLIO_DIR" && pnpm exec lint-staged
+fi
+
+if git diff --cached --quiet -- "$PORTFOLIO_SRC_DIR"; then
+  echo "No changes in $PORTFOLIO_SRC_DIR, skipping pre-commit."
+else
+  echo "Changes detected in $PORTFOLIO_SRC_DIR, running pre-commit script."
+  cd "$PORTFOLIO_DIR" && pnpm test && pnpm knip && pnpm exec lint-staged
 fi
--- portfolio/lint-staged.config.js
+++ portfolio/lint-staged.config.js
@@ -1,3 +1,10 @@
+const runPrettierOnNonTsFiles = (files) => {
+  const excludedExtensions = ['.sh', '.ts', '.tsx'];
+
+  return files.filter((file) => !excludedExtensions.some((ext) => file.endsWith(ext))).map((file) => `prettier --write "${file}"`);
+};
+
 export default {
+  '*': runPrettierOnNonTsFiles,
   '*.{ts,tsx}': ['prettier --write', 'eslint', () => 'tsc']
 };
