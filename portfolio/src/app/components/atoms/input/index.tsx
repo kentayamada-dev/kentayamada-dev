0cc9c74f59c85737b1ede6bd6f28c650c28aa4da
Kenta Yamada <kentayamada058@gmail.com>
2025-03-19 01:18:11 -0400

    fix auto-zoom in safari

--- portfolio/src/app/components/atoms/input/index.tsx
+++ portfolio/src/app/components/atoms/input/index.tsx
@@ -6,11 +6,11 @@ const Input: InputType = (props) => {
 
   return (
     <div>
-      <label className='text-primary block pb-2 text-sm font-medium' htmlFor={inputProps.id}>
+      <label className='text-primary block pb-2 text-base font-medium' htmlFor={inputProps.id}>
         {label}
       </label>
       <div className='overflow-hidden rounded-lg ring-1 ring-gray-300 has-[input:focus-within]:ring-2 has-[input:focus-within]:ring-blue-500 dark:ring-gray-600'>
-        <input {...inputProps} className='bg-primary w-full p-2.5 text-sm text-black focus:outline-hidden dark:text-white' />
+        <input {...inputProps} className='bg-primary w-full p-2.5 text-base text-black focus:outline-hidden dark:text-white' />
       </div>
     </div>
   );
--- portfolio/src/app/components/atoms/inputWithCombobox/index.tsx
+++ portfolio/src/app/components/atoms/inputWithCombobox/index.tsx
@@ -7,15 +7,15 @@ const InputWithCombobox: InputWithComboboxType = (props) => {
 
   return (
     <div>
-      <label className='text-primary block pb-2 text-sm font-medium' htmlFor={id}>
+      <label className='text-primary block pb-2 text-base font-medium' htmlFor={id}>
         {label}
       </label>
       <div>
         <div className='flex overflow-hidden rounded-lg ring-1 ring-gray-300 has-[input:focus-within]:ring-2 has-[input:focus-within]:ring-blue-500 dark:ring-gray-600'>
-          <input {...inputProps} className='bg-primary grow p-2.5 text-sm text-black focus:outline-hidden dark:text-white' id={id} />
+          <input {...inputProps} className='bg-primary grow p-2.5 text-base text-black focus:outline-hidden dark:text-white' id={id} />
           <div className='grid shrink-0 grid-cols-1'>
             <select
-              className='bg-primary col-start-1 row-start-1 appearance-none rounded-l-none rounded-r-lg py-1.5 pr-7 pl-3 text-sm text-black hover:cursor-pointer focus:outline-2 focus:-outline-offset-2 dark:text-white'
+              className='bg-primary col-start-1 row-start-1 appearance-none rounded-l-none rounded-r-lg py-1.5 pr-7 pl-3 text-base text-black hover:cursor-pointer focus:outline-2 focus:-outline-offset-2 dark:text-white'
               onChange={handleOptionChange}
               value={optionValue}
             >
