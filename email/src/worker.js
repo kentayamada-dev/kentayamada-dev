deff47c3b6ba3de214b083d40480b84cc2057a18
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2025-06-22 03:59:30 -0400

    Update worker.js

--- email/src/worker.js
+++ email/src/worker.js
@@ -10,22 +10,19 @@ export default {
     const resend = new Resend(env.RESEND_API_KEY);
 
     const headerTable = `
-    <>
-      <table style="border:1px solid oklch(0.446 0.043 257.281);border-collapse:collapse;font-size:16px">
-        <tbody>
-          <tr>
-            <td style="border:1px solid oklch(0.446 0.043 257.281);font-weight:700;padding:8px">Sender</td>
-            <td style="border:1px solid oklch(0.446 0.043 257.281);padding:8px">${email.from.name} ${email.from.address}</td>
-          </tr>
-          <tr>
-            <td style="border:1px solid oklch(0.446 0.043 257.281);font-weight:700;padding:8px">Subject</td>
-            <td style="border:1px solid oklch(0.446 0.043 257.281);padding:8px">${email.subject}</td>
-          </tr>
-        </tbody>
-      </table>
-      <br>
-      <br>
-    </>
+    <table style="border:1px solid oklch(0.446 0.043 257.281);border-collapse:collapse;font-size:16px">
+      <tbody>
+        <tr>
+          <td style="border:1px solid oklch(0.446 0.043 257.281);font-weight:700;padding:8px">Sender</td>
+          <td style="border:1px solid oklch(0.446 0.043 257.281);padding:8px">${email.from.name} ${email.from.address}</td>
+        </tr>
+        <tr>
+          <td style="border:1px solid oklch(0.446 0.043 257.281);font-weight:700;padding:8px">Subject</td>
+          <td style="border:1px solid oklch(0.446 0.043 257.281);padding:8px">${email.subject}</td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
   `;
 
     await resend.emails.send({
