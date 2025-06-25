8ea1b174e8a031d1b71a09a897d2a875d5c0b2ab
Kenta Yamada <kentayamada058@gmail.com>
2025-06-24 22:47:21 -0400

    update github actions

---
+++ .github/workflows/scripts/deploy_storybook.sh
@@ -0,0 +1,9 @@
+apt update && apt --yes install --no-install-recommends nodejs npm curl
+npm install --global vercel
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=$2 SHELL=/bin/bash bash -
+. /root/.bashrc
+pnpm env use --global $1
+cd ./app/portfolio
+pnpm build-storybook
+cd ./.storybook/output
+vercel deploy --prebuilt --prod --token=$3
