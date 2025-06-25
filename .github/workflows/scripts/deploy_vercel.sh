cf91ee62f3d47fd77b588743249d6d5b0877f81a
Kenta Yamada <kentayamada058@gmail.com>
2025-06-24 22:13:36 -0400

    update

--- .github/workflows/scripts/deploy_vercel.sh
+++ .github/workflows/scripts/deploy_vercel.sh
@@ -3,11 +3,7 @@ npm install --global vercel
 curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=$2 SHELL=/bin/bash bash -
 . /root/.bashrc
 pnpm env use --global $1
-
-vercel --cwd ./app/portfolio
+cd ./app/portfolio
 vercel pull --yes --environment=production --token=$3
 vercel build --prod --token=$3
 vercel deploy --prebuilt --prod --token=$3
-
-# vercel --cwd ./app/portfolio/.storybook/output
-# vercel deploy --prebuilt --prod --token=$3
